99472a68ef32691338386e6d0e9e32e5ff8e75a2
areeb-h <areeb.hw@gmail.com>
2022-11-07 09:32:28 +0500

    footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,15 +1,20 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link'
 
 export default function Footer() {
     return (
-        <div className='shadow-sm  py-4 px-5 flex justify-between mb-5 bg-white items-center static bottom-0'>
+        <div className='shadow-sm  py-4 px-5 grid grid-cols-3 mb-5 bg-white items-center static bottom-0'>
             <div>
                 <h1>Hulhumale Academic Center</h1>
-                <p></p>
+                <p>Goalhi 34543</p>
+                <p>Hulhumale'</p>
             </div>
-            <div>
-                Hulhumale Academic Center
+            <div className="block h-full bg-slate-400">
+                <Link href='/'>aa</Link>
+            </div>
+            <div className="flex h-full items-start justify-end">
+                <Link href='/' className="px-4 py-2 bg-gray-800 text-white rounded-lg">Contact</Link>
             </div>
         </div>
     )
