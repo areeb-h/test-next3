b22a5e198e45e04699c25ab83c76c343bbf6d214
areeb-h <areeb.hw@gmail.com>
2022-11-10 11:36:46 +0500

    index

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -15,7 +15,7 @@ var contactLinkStyle = 'px-2 py-1 bg-gray-100 text-gray-800 rounded-lg hover:bg-
 export default function Footer() {
     return (
         <div className=' flex-rows items-center justify-center md:flex md:justify-between bg-white static bottom-0 '>
-            <div className=' md:pl-5 py-5 block text-center md:text-start'>
+            <div className=' md:pl-5 py-4 block text-center md:text-start'>
                 <a className='text-gray-800 flex-rows hover:text-black' target="_blank" href='https://g.page/hulhumale-academic-centre?share' rel="noopener noreferrer">
                     <h1 className="text-md font-bold">Hulhumale&apos; Academic Center</h1>
                     <p>Hithigas Magu, Goalhi 3</p>
@@ -29,7 +29,8 @@ export default function Footer() {
                 </div>   
                 <div className=' flex gap-x-3 justify-center md:justify-start items-center pt-3 '> 
                     <Image width={30} src={phone} className="hidden lg:block " alt='phone icon'/>
-                    <Link href='Tel: +9609948882' className={contactLinkStyle}>+960 335-5715</Link>
+                    <p className="">+960</p>
+                    <Link href='Tel: +9609948882' className={contactLinkStyle}>335-5715</Link>
                     <Link href='Tel: +9609948882' className={contactLinkStyle}>793-5717</Link>
                 </div>                         
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
+import Link from 'next/link'
+
+var contactLinkStyle = ' bg-teal-700 text-2xl text-white rounded-2xl hover:bg-teal-500 ml-0 transform transition duration-200 ease-in-out'
+
 export default function Home() {
-  return <div className="h-screen flex justify-center gap gap-x-8	">
-    <div className=" bg-blue-500 p-4 ">
-      a
+  return <div className=" block max-w-5xl mx-auto">
+    <div className=" flex-columns items-center justify-center md:flex space-y-5 md:space-y-0 gap-y-5 md:gap-x-5	">
+      <div className=" md:flex-grow md:w-8/12 tracking-wide bg-green-100 border-4 border-green-200 p-5 rounded-xl">
+        <p className=" leading-relaxed text-xl text-gray-800">
+          <span className=" font-semibold ">HAC</span> is the first private education institution established in Hulhumale’. We place a heavy focus on teaching excellence and providing a place where your children can feel at home while learning the skills that will help them thrive long after their school years are over. 
+          As a parent, what more can you ask for?
+        </p>
+      </div>
+      <div className=" flex-rows items-center justify-center md:w-5/12 ">
+        <div className=' flex-columns justify-center mb-5'>
+          <div className=' flex justify-center'>
+            <p className=" leading-relaxed text-3xl text-center text-gray-800 "> Registrations Open for </p>
+          </div>
+          <div className=' flex justify-center'>
+            <p className=" leading-relaxed text-3xl font-bold text-gray-800 "> 2022-2023 </p>
+          </div>
+        </div>
+        <div className=' flex justify-center'>
+          <Link href='/register' className={contactLinkStyle + " flex justify-center items-center "}>
+            <div className='px-5 pt-3 pb-4'>
+              Get Started
+            </div>
+            <div className="bg-teal-600 px-2 py-2 mr-2 rounded-xl text-xl">
+              ➜
+            </div>
+          </Link>
+        </div>
+      </div>
     </div>
-    <div  className=" bg-green-500 p-4 ">
-      a
-   </div>
   </div>
 }
