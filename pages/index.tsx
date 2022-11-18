44b571bbd66d1afbee025554eced782bb46e9b3a
areeb-h <areeb.hw@gmail.com>
2022-11-18 17:03:51 +0500

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import Link from 'next/link'
+import hac from './assets/hac/hac.png'
+import ucmas from './assets/ucmas.png'
+import Image from 'next/image'
 
-var contactLinkStyle = ' bg-teal-700 md:leading-relaxed text-xl md:text-2xl text-white rounded-2xl hover:bg-teal-500 ml-0 transform transition duration-200 ease-in-out'
+var contactLinkStyle = ' bg-teal-700 text-xl md:text-2xl text-white rounded-2xl hover:bg-teal-500 ml-0 transform transition duration-200 ease-in-out'
+
+var viewMoreButton = ' bg-cyan-700 px-5 py-2 text-md md:text-lg text-white rounded-xl hover:bg-cyan-600 ml-0 flex justify-center items-center transform transition duration-200 ease-in-out'
+
 
 export default function Home() {
   return <div className=" block max-w-5xl mx-auto">
@@ -23,11 +29,39 @@ export default function Home() {
         <div className=' flex justify-center'>
           <Link href='/register' className={contactLinkStyle + " flex justify-center items-center "}>
             <div className='pl-5 py-3 '>
-              Get Started
+              Get Enrolled
             </div>
             <div className="bg-teal-600 px-2 py-0.5 mx-3 rounded-xl text-xl">
               ➜
             </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+    <div className=' flex-columns items-center justify-center md:flex space-x-0 md:space-x-5 space-y-10 md:space-y-0 mb-20'>
+      <div className=' bg-white rounded-2xl shadow-xl basis-1/2 '>
+        <div className=' p-5 md:p-10 pb-0 md:pb-0 flex justify-between items-center'>
+          <h1 className=' text-2xl font-bold mr-4'>Cambridge English</h1>
+          <div className=' bg-gray-100 p-4 rounded-2xl'><Image className='w-ucmas ' src={hac} alt='user profile picture'/> </div>
+        </div>
+        <p className=' text-xl p-5 md:p-10'>We offer a number of programs for students from ages 4 through 12 and above, up to Cambridge IGCSE O’level English, as well as classes for adults.</p>
+        <div className='bg-gray-50 flex p-5 md:px-10 justify-between rounded-b-2xl items-center'>
+          <h1 className=' text-lg font-bold'> 13 Courses</h1>
+          <Link href='/register' className={viewMoreButton}>
+            <div className=' text-lg'> View All </div>
+          </Link>
+        </div>
+      </div>
+      <div className=' bg-white rounded-2xl shadow-xl basis-1/2'>
+        <div className=' p-5 md:p-10 pb-0 md:pb-0 flex justify-between items-center'>
+          <h1 className=' text-2xl font-bold mr-4'>UCMAS Mental Math</h1>
+          <div className=' bg-gray-100 p-4 rounded-2xl'><Image className='w-ucmas ' src={ucmas} alt='user profile picture'/> </div>
+        </div>
+          <p className=' text-xl p-5 md:p-10'>This is a mind developing program where students improve different skills that will help in learning other subjects too. This program is for ages 4 to 14</p>
+        <div className='bg-gray-50 flex p-5 md:px-10 justify-between rounded-b-2xl items-center'>
+          <h1 className=' text-lg font-bold'> 13 Courses</h1>
+          <Link href='/register' className={viewMoreButton}>
+            <div className=' text-lg'> View All  </div>
           </Link>
         </div>
       </div>
